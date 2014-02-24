6f9004f78e70e36fbd7ea6e13fc103eb20899ebf
oopdsc <oopdsc@gmail.com>
2014-02-24 23:43:12 +0800

    aumaterial service

---
+++ BGMG/src/service/AuMaterialService.java
@@ -0,0 +1,33 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.material.AuxiliaryMaterial;
+import model.material.MaterialItem;
+
+public class AuMaterialService {
+	
+	private List<AuxiliaryMaterial> list;
+	
+	public AuMaterialService(){
+		list = new ArrayList<AuxiliaryMaterial>();
+	}
+	
+	public void add(AuxiliaryMaterial item){
+		if(!list.contains(item)){
+			list.add(item);
+		}
+	}
+	
+	public void remove(AuxiliaryMaterial item){
+		if(list.contains(item)){
+			list.remove(item);
+		}
+	}
+	
+	public List<AuxiliaryMaterial> getAllRecords(){
+		return list;
+	}
+
+}
